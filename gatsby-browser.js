aa625c459399d9e619b2b308d565cc8e7420a792
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-05-07 01:26:10 +0530

    styling and boostrap

---
+++ gatsby-browser.js
@@ -0,0 +1,6 @@
+
+// ADDING BOOTSTRAP
+import "bootstrap/dist/css/bootstrap.min.css";
+import "jquery/dist/jquery.min.js";
+import "popper.js/dist/popper.min";
+import "bootstrap/dist/js/bootstrap.min.js";
