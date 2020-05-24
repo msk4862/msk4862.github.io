216e65cd6b9e47f0007e71f51c081c46e6595e35
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-05-25 00:26:59 +0530

    Updates

---
+++ component---src-pages-404-js-a45e6b66a3fff398d50b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(n,e,t){"use strict";t.r(e);var u=t("q1tI"),w=t.n(u);e.default=function(){return w.a.createElement("div",null,"404")}}}]);
+//# sourceMappingURL=component---src-pages-404-js-a45e6b66a3fff398d50b.js.map
