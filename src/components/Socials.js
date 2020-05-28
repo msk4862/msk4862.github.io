a6525cc3f6745d77640a30fe92c3cbabc14408e0
Shoaib Asgar <shoaib.mca19.du@gmail.com>
2020-05-28 13:33:15 +0530

    socials styles fixes

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -6,13 +6,16 @@ const Socials = () => {
     return (
         <div className="row social-links justify-content-center">
             <a href={SocialLinks.linkedin} target="blank">
-                <i className="zoom fa fa-linkedin-square fa-2x"></i>
+                <i className="zoom fa fa-linkedin-square"></i>
             </a>
             <a href={SocialLinks.github} target="blank">
-                <i className="zoom fa fa-github fa-2x"></i>
+                <i className="zoom fa fa-github"></i>
             </a>
             <a href={SocialLinks.twitter} target="blank">
-                <i className="zoom fa fa-twitter-square fa-2x"></i>
+                <i className="zoom fa fa-twitter-square"></i>
+            </a>
+            <a href={`mailto:${SocialLinks.email}`}>
+                <i className="zoom fa fa-envelope"></i>
             </a>
         </div>
     )
